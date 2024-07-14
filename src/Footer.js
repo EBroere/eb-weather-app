a535b40d95e692594077ee0dab9c3093a3b83553
EBroere <e.broere91@gmail.com>
2024-07-14 16:17:37 +0200

    updated code via terminal

--- src/Footer.js
+++ src/Footer.js
@@ -9,18 +9,24 @@ export default function Footer() {
           <a
             href="https://www.linkedin.com/in/ellen-broere-bb025386/"
             target="_blank"
+            rel="noreferrer"
           >
             <strong>Ellen Broere</strong>
           </a>
-          <span class="footer-emoji">🧍🏽‍♀️ </span>
+          <span className="footer-emoji">🧍🏽‍♀️ </span>
           and is open-sourced on{" "}
-          <a href="https://github.com/EBroere/eb-weather-app" target="_blank">
+          <a
+            href="https://github.com/EBroere/eb-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
             GitHub{" "}
           </a>
           and hosted on{" "}
           <a
             href="https://eb-weather-app-shecodes.netlify.app/"
             target="_blank"
+            rel="noreferrer"
           >
             Netlify
           </a>
